1e96b6cc9dcbd2d5da3b33b5f7a4f98918278693
payocoder <52519968+payocoder@users.noreply.github.com>
2020-09-25 18:56:29 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Krunker.io Client
+// @description  Krunker.io Client
+// @version      0.6.0
+// @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io(|\/|\/\?.+)$/
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+document.addEventListener('DOMContentLoaded', _ => {
+	alert("To get the client, join the discord: https://discord.gg/MhBfMpH");
+}, false);
